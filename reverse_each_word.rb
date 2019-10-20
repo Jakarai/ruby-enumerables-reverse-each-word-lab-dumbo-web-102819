1c79fac2573a57664238f387549174859579f964
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-20 06:26:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,6 +5,6 @@ def reverse_each_word(sentence1)
     sentence_array.each do |sentence1|
       new_sentence << sentence1.reverse
     end
-    new_sentence.join(" ")
+    new_sentence.join("")
     
 end
