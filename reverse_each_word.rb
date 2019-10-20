232586db09a5503fd3f9916982f2a896f12f6ef7
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-20 03:07:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,9 @@ def reverse_each_word(sentence1)
   i = 0 
   new_array = []
   while i < sentence1.length do
-    sentence1.reverse(0) 
+    new_array.push(sentence1.reverse(0))
     i +=1 
   end
+  new_array
   
 end
